9f27ad97c4c757c8a17dfae853c1dd6db86d1ce4
BelousovaDarra <darenok95@mail.ru>
2015-06-25 09:38:35 +0500

    Hi
    Hi

---
+++ Белоусова/А1А2/А1А2/Employed.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace А1А2
+{
+    public partial class Employed : Form
+    {
+        public Employed()
+        {
+            InitializeComponent();
+        }
+
+        public EmployedData SSS { get; set; }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SSS = new EmployedData();
+            SSS.Start = dateTimePicker1.Value;
+            SSS.End = dateTimePicker2.Value;
+            SSS.Obshe = textBox1.Text;
+            SSS.Komn = textBox2.Text;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Белоусова/числаБелоусова/числаБелоусова/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace числаБелоусова
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64 min = 0, max = int.MaxValue, x = max / 2, z=0, f=0;
+            Console.WriteLine("Загадайте число от 0 до " + max + "\nДля ответов на вопросы используйте команды 1-да 0-нет");
+
+            while (z==0)
+            {
+                Console.WriteLine("Ваше число меньше " + x + "?");
+                if (Console.ReadLine()[0] == '1')
+                {
+                    max = x;
+                    x = max / 2 + min / 2;
+                }
+                else
+                {
+                    min = x;
+                    x = max / 2 + min / 2;
+                  
+                }
+               
+                    x = (max + min) / 2;
+                if (x == min)
+                {
+                    z=1;
+                }
+            }
+
+            Console.WriteLine("Вы загадали " + x + "?");
+            if (Console.ReadLine()[0] == '1')
+            {
+                Console.WriteLine("Ура!");
+            }
+            else
+            {
+                Console.WriteLine("Не возможно");
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
